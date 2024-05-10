3cfe54fa942c79fcbdb91b0de11ccb94d779d3c5
Maria Mazza <mmazza@fsu.edu>
2024-05-10 13:30:21 -0700

    Add VVHjj samples for condor submission

--- skimmer/condor/samples.py
+++ skimmer/condor/samples.py
@@ -1,19 +1,28 @@
 from metis.Sample import DirectorySample, DBSSample
+from vbsvvh_data import nanoaodv9_data
+from vbsvvh_mc import nanoaodv9_bkg
+
 
 # Master list of all samples
 # Specify a dataset name and a short name for the output root file on nfs
 
-samples_to_submit = [
+# samples_to_submit = [
 
-    # Data
-    DBSSample(dataset="/Muon/Run2022G-PromptNanoAODv11_v1-v2/NANOAOD"),
-    DBSSample(dataset="/EGamma/Run2022G-PromptNanoAODv11_v1-v2/NANOAOD"),
-    DBSSample(dataset="/MuonEG/Run2022G-PromptNanoAODv11_v1-v2/NANOAOD"),
+#     # Data
+#     DBSSample(dataset="/Muon/Run2022G-PromptNanoAODv11_v1-v2/NANOAOD"),
+#     DBSSample(dataset="/EGamma/Run2022G-PromptNanoAODv11_v1-v2/NANOAOD"),
+#     DBSSample(dataset="/MuonEG/Run2022G-PromptNanoAODv11_v1-v2/NANOAOD"),
 
-    # MC
-    DBSSample(dataset="/DYto2L-2Jets_MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
-    DBSSample(dataset="/WW_TuneCP5_13p6TeV_pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
-    DBSSample(dataset="/WtoLNu-2Jets_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
-    DBSSample(dataset="/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
+#     # MC
+#     DBSSample(dataset="/DYto2L-2Jets_MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
+#     DBSSample(dataset="/WW_TuneCP5_13p6TeV_pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
+#     DBSSample(dataset="/WtoLNu-2Jets_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
+#     DBSSample(dataset="/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EENanoAODv11-126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM"),
 
-]
+# ]
+
+
+samples_to_submit = []
+# data 
+samples_to_submit += nanoaodv9_data
+print(samples_to_submit)
---
+++ skimmer/condor/vbsvvh_data.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+from metis.Sample import DBSSample
+
+# DIS searches:
+# JetHT Run2: /JetHT/*UL*MiniAODv2_NanoAODv9*/NANOAOD
+
+nanoaodv9_data = [
+
+    DBSSample(dataset="/JetHT/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2016C-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2016D-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2016E-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2016F-HIPM_UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+    
+    DBSSample(dataset="/JetHT/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2016H-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+
+    DBSSample(dataset="/JetHT/Run2017B-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2017C-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2017D-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2017E-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2017F-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+
+    DBSSample(dataset="/JetHT/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2018B-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2018C-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+    DBSSample(dataset="/JetHT/Run2018D-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+
+    # DBSSample(dataset="/JetHT/Run2018A-UL2018_MiniAODv2_NanoAODv9_GT36-v1/NANOAOD"),
+    # DBSSample(dataset="/JetHT/Run2018B-UL2018_MiniAODv2_NanoAODv9_GT36-v1/NANOAOD"),
+    # DBSSample(dataset="/JetHT/Run2018C-UL2018_MiniAODv2_NanoAODv9_GT36-v1/NANOAOD"),
+]
