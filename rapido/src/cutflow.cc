49c661e9fa60db4ed4a0c6e83a0ef5134dc4c409
Maria Mazza <mmazza@fsu.edu>
2024-07-11 16:53:54 -0700

    Add jguiang changes

--- rapido/src/cutflow.cc
+++ rapido/src/cutflow.cc
@@ -15,6 +15,11 @@ Cut::Cut(std::string new_name)
 
 Cut::~Cut() {}
 
+Cut *Cut::clone(std::string new_name)
+{
+    return new Cut(new_name);
+}
+
 void Cut::print()
 {
     std::cout << "---- " << name << " ----" << std::endl;
@@ -492,7 +497,6 @@ void Cutflow::recursivePrint(std::string tabs, Cut* cut, Direction direction, bo
             if (direction == Right) { identical_yield = (cut->n_pass == cut->parent->n_pass); }
             else { identical_yield = (cut->n_pass == cut->parent->n_fail); }
         }
-        if (direction == Right)
         if (has_parent || !identical_yield)
         {
             // Print cut info
