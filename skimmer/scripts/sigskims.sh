010eba995b99ef9b2b0e878ac800c302e2544cb0
Maria Mazza <mmazza@fsu.edu>
2025-11-11 13:24:52 -0800

    Edit subdir name to match xsec file in RDF

--- skimmer/scripts/sigskims.sh
+++ skimmer/scripts/sigskims.sh
@@ -1,29 +1,31 @@
-
 # %
-sig_tag="-run2-c2v-jguiang"     #"v2"                 #"run2-c2v-jguiang"
-sig_job_dir="run2_c2v_jguiang"  #${sig_tag}"_merged"  #"run2_c2v_jguiang"
-sig_production_tag="v2"
+sig_tag="VBSVVH_VBSCuts_13TeV_4f_LO_MG_2_9_18_c2v_1p5_c3_1p0_c2Vc3scan_slc7_amd64_gcc10_CMSSW_12_4_8" #"v2"     #                 #"-run2-c2v-jguiang"
+sig_job_dir=${sig_tag} #${sig_tag}"_merged"  #"run2_c2v_jguiang"
+sig_production_tag="" #"_v2"
+nanoaod_affix="NANOGEN" #"-106X_privateMC_NANOGEN"
+nanoaod_suffix="VBSCuts_13TeV" #"VBSCuts"
 signal_samples_dir="/ceph/cms/store/user/mmazza/SignalGeneration/" #"/ceph/cms/store/user/jguiang/VBSVVHSignalGeneration/v2/" #
 SAMPLES="
-${signal_samples_dir}/${sig_job_dir}/VBSWZH_VBSCuts_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWZH_VBSCuts_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWZH_VBSCuts_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWZH_VBSCuts_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSZZH_VBSCuts_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSZZH_VBSCuts_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSZZH_VBSCuts_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSZZH_VBSCuts_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_VBSCuts_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_VBSCuts_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_VBSCuts_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_VBSCuts_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_VBSCuts_TuneCP5_RunIISummer20UL16-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_VBSCuts_TuneCP5_RunIISummer20UL16APV-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_VBSCuts_TuneCP5_RunIISummer20UL17-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
-${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_VBSCuts_TuneCP5_RunIISummer20UL18-106X_privateMC_NANOGEN_${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16APV_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16APV_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_TuneCP5_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16APV${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL16APV_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_TuneCP5_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
 "
-ANALYSIS_TAG=LepVeto
-SKIMTAG=nanoaodv9_sig${sig_tag}_${ANALYSIS_TAG}_5Feb2025
+ANALYSIS_TAG=None
+new_suffix="C2V_1p0_C3_10p0_13TeV_4f_LO"
+SKIMTAG=nanoaodv9_sig_cuts-${ANALYSIS_TAG}_${sig_tag}_11Nov2025_v2
 SKIMDIR=/ceph/cms/store/user/mmazza/skim/${SKIMTAG}/ 
 mkdir -p $SKIMDIR
 
@@ -35,15 +37,66 @@ if [[ "$SAMPLES" != "" && "$SKIMDIR" != "" && "$SKIMTAG" != "" ]]; then
         echo "Skimming $sample" >> $outLogFile
         ./$SKIMMER -t Events -d . -a $ANALYSIS_TAG -n output -T Events $sample --dump_truth --is_signal>> $outLogFile
         sampledir=$(dirname $sample)
-        targetdir=$SKIMDIR/${sampledir##*/}
+        original_subdir=${sampledir##*/}
+        new_subdir=${original_subdir/${nanoaod_suffix}/${new_suffix}}
+        targetdir=$SKIMDIR/$new_subdir
         mkdir -p $targetdir
         mv output.root $targetdir/merged.root
         mv cutflow.txt $targetdir/.
         echo "Wrote skim to $targetdir/merged.root" >> $outLogFile
-        echo "ls $$targetdir" >> $outLogFile
+        echo "ls $targetdir" >> $outLogFile
         ls $targetdir >> $outLogFile
     done
 fi
+
+
+# # %
+# sig_tag="VBSVVH_VBSCuts_13TeV_4f_LO_MG_2_9_18_c2v_1p5_c3_1p0_c2Vc3scan_slc7_amd64_gcc10_CMSSW_12_4_8" #"v2"     #                 #"-run2-c2v-jguiang"
+# sig_job_dir=${sig_tag} #${sig_tag}"_merged"  #"run2_c2v_jguiang"
+# sig_production_tag="" #"_v2"
+# nanoaod_affix="NANOGEN" #"-106X_privateMC_NANOGEN"
+# nanoaod_suffix="VBSCuts_13TeV_TuneCP5" #"VBSCuts_TuneCP5"
+# signal_samples_dir="/ceph/cms/store/user/mmazza/SignalGeneration/" #"/ceph/cms/store/user/jguiang/VBSVVHSignalGeneration/v2/" #
+# SAMPLES="
+# ${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_RunIISummer20UL16APV_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWZH_${nanoaod_suffix}_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_RunIISummer20UL16APV_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSZZH_${nanoaod_suffix}_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_RunIISummer20UL16APV${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_SS_${nanoaod_suffix}_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_RunIISummer20UL16_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_RunIISummer20UL16APV_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_RunIISummer20UL17_${nanoaod_affix}${sig_production_tag}/merged.root
+# ${signal_samples_dir}/${sig_job_dir}/VBSWWH_OS_${nanoaod_suffix}_RunIISummer20UL18_${nanoaod_affix}${sig_production_tag}/merged.root
+# "
+# ANALYSIS_TAG=None
+# SKIMTAG=nanoaodv9_sig_cuts-${ANALYSIS_TAG}_${sig_tag}_11Nov2025
+# SKIMDIR=/ceph/cms/store/user/mmazza/skim/${SKIMTAG}/ 
+# mkdir -p $SKIMDIR
+
+# SKIMMER=skim
+# #echo "Begin " > $outLogFile
+# outLogFile=${SKIMDIR}/skimmer_out.log
+# if [[ "$SAMPLES" != "" && "$SKIMDIR" != "" && "$SKIMTAG" != "" ]]; then
+#     for sample in $SAMPLES; do
+#         echo "Skimming $sample" >> $outLogFile
+#         ./$SKIMMER -t Events -d . -a $ANALYSIS_TAG -n output -T Events $sample --dump_truth --is_signal>> $outLogFile
+#         sampledir=$(dirname $sample)
+#         targetdir=$SKIMDIR/${sampledir##*/}
+#         mkdir -p $targetdir
+#         mv output.root $targetdir/merged.root
+#         mv cutflow.txt $targetdir/.
+#         echo "Wrote skim to $targetdir/merged.root" >> $outLogFile
+#         echo "ls $$targetdir" >> $outLogFile
+#         ls $targetdir >> $outLogFile
+#     done
+# fi
 
 
 # SKIMDIR=/data/userdata/jguiang/nanoaod/forMaria/new
